7cdbc43c20f6c527586ed1c69db34338ac1a9bed
vinasalsa1 <vinasalsabila65@gmail.com>
2025-07-12 09:19:06 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,3 +12,15 @@ st.subheader("_Streamlit_ is :blue[cool] :sunglasses:")
 st.markdown("*Streamlit* is **really** ***cool***.")
 
 st.metric(label="Temperature", value="70 °F", delta="1.2 °F")
+
+with col1:
+    st.header("A cat")
+    st.image("https://static.streamlit.io/examples/cat.jpg")
+
+with col2:
+    st.header("A dog")
+    st.image("https://static.streamlit.io/examples/dog.jpg")
+
+with col3:
+    st.header("An owl")
+    st.image("https://static.streamlit.io/examples/owl.jpg")
