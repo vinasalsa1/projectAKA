aedd5dd0f152792962f859ecb7d461c7e9234511
vinasalsa1 <vinasalsabila65@gmail.com>
2025-07-15 12:04:08 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -161,7 +161,4 @@ add_selectbox = st.sidebar.selectbox(
 
 # Using "with" notation
 with st.sidebar:
-    add_radio = st.radio(
-        "Choose a shipping method",
-        ("Standard (5-15 days)", "Express (2-5 days)")
-    )
+  
